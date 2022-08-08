887f8f6ec1bb5ea10cbeea0f44165c599b80ef64
asrarbutt <asrarbutt1986@gmail.com>
2022-08-08 15:42:22 +0200

    initial commit

---
+++ src/main/java/com/example/springbootsecuritybasic/SpringBootSecurityBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootsecuritybasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityBasicApplication.class, args);
+    }
+
+}
