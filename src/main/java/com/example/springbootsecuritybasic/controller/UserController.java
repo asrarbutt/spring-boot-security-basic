e7fd567c7939ef246e39cbd7e815a4f0a51033bc
asrarbutt <asrarbutt1986@gmail.com>
2022-08-08 16:01:34 +0200

    finish UserController

--- src/main/java/com/example/springbootsecuritybasic/controller/UserController.java
+++ src/main/java/com/example/springbootsecuritybasic/controller/UserController.java
@@ -1,10 +1,37 @@
 package com.example.springbootsecuritybasic.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("api/users")
 public class UserController {
 
+
+    @GetMapping("login")
+    public String login(){
+
+        return me();
+    }
+
+    @GetMapping("me")
+    String me(){
+        return ((User) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal()).getUsername();
+
+
+
+    }
+
+    @GetMapping("logout")
+    void logout(HttpSession session){
+        session.invalidate();
+    }
 }
