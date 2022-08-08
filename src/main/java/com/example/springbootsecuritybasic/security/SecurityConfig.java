334158d60353814264b0aafc0877a8ee0fa0731c
asrarbutt <asrarbutt1986@gmail.com>
2022-08-08 15:42:39 +0200

    add securityConfig class

---
+++ src/main/java/com/example/springbootsecuritybasic/security/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.example.springbootsecuritybasic.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+}
