adbea8d5e36eae1dbbde443ab328834324113725
asrarbutt <asrarbutt1986@gmail.com>
2022-08-08 15:50:01 +0200

    finish SecurityConfig

--- src/main/java/com/example/springbootsecuritybasic/security/SecurityConfig.java
+++ src/main/java/com/example/springbootsecuritybasic/security/SecurityConfig.java
@@ -1,8 +1,34 @@
 package com.example.springbootsecuritybasic.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("frank")
+                .password(passwordEncoder().encode("Hallo123"))
+                .roles("ADMIN");
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/api/user/*").permitAll()
+                .antMatchers("api/**").authenticated()
+                .and().httpBasic();
+    }
 }
