6df73c29d2048feb964b601e90f9376e2e7bfd3e
asrarbutt <asrarbutt1986@gmail.com>
2022-08-08 16:27:38 +0200

    finish security configuration

--- src/main/java/com/example/springbootsecuritybasic/controller/HelloController.java
+++ src/main/java/com/example/springbootsecuritybasic/controller/HelloController.java
@@ -1,4 +1,15 @@
 package com.example.springbootsecuritybasic.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class HelloController {
+
+    @GetMapping("/hallo")
+    String hallo(){
+        return "hallo";
+    }
 }
--- src/main/java/com/example/springbootsecuritybasic/security/SecurityConfig.java
+++ src/main/java/com/example/springbootsecuritybasic/security/SecurityConfig.java
@@ -3,12 +3,13 @@ package com.example.springbootsecuritybasic.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -17,7 +18,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("frank")
                 .password(passwordEncoder().encode("Hallo123"))
                 .roles("ADMIN");
-
     }
 
     @Bean
